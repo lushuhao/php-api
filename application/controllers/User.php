5cdb0330e991684d158df309640f8743a3907c54
ShuhaoLu <ShahramLu@163.com>
2017-08-20 23:24:50 +0800

    用户注册与登录

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -10,8 +10,54 @@ class UserController extends Yaf_Controller_Abstract {
         return $this->loginAction();
     }
 
+    /**
+     * 用户登录
+     * @return bool
+     */
     public function loginAction() {
+        $submit = $this->getRequest()->getQuery("submit", '0');// submit没有传，默认0，额外验证，防止程序登录
+        if ($submit!='1'){
+            echo json_encode(array('errno'=>-1001, 'errmsg'=>'请通过正确渠道提交'));
+            return false;
+        }
+
+        // 获取参数
+        $uname = $this->getRequest()->getPost('uname', false);
+        $pwd = $this->getRequest()->getPost('pwd', false);
+        if (!$uname || !$pwd){
+            echo json_encode(array('errno'=>-1002,'errmsg'=>'用户名与密码必须传递'));
+            return false;
+        }
+
+        // 调用Model，做登录验证
+        $model = new UserModel();
+        $uid = $model->login(trim($uname), trim($pwd));
+        if ($uid){
+            // 种Session
+            /*
+            session_start(); // 初始化session数据，在客户端生成cookie，服务端生成存放session数据的临时文件
+            $_SESSION['user_token'] = md5("salt".$_SERVER['REQUEST_TIME'].$uid); // 服务器端的cookie，运行期间可以使用
+            $_SESSION['user_token_time'] = $_SERVER['REQUEST_TIME']; // 请求开始时的时间戳
+            $_SESSION['user_id'] = $uid;
+            */
+            echo json_encode(array(
+                'errno'=>0,
+                'errmsg'=>'',
+                'data'=>array('name'=>$uname)
+            ));
+        } else {
+            echo json_encode(array(
+                'errno'=>$model->errno,
+                'errmsg'=>$model->errmsg
+            ));
+        }
+        return true;
     }
+
+    /**
+     * 用户注册
+     * @return bool
+     */
 	public function registerAction() {
 		// 获取参数
 		$uname = $this->getRequest()->getPost("uname", false); //getRequst()是父类，返回实例，getPost(第二个参数可选是在未找到的情况下返回)
--- application/models/User.php
+++ application/models/User.php
@@ -10,11 +10,61 @@
 class UserModel {
     public $errno = 0;
     public $errmsg = '';
+    private $_db = null;
 
     public function __construct() {
+        $this->_db =  new PDO('mysql:host=127.0.0.1;dbname=imooc;','root', '342623');
+    }
+
+    public function login($uname, $pwd){
+        $query = $this->_db->prepare('select `pwd`,`id` from `user` WHERE `name`= ?');
+        $query->execute(array($uname));
+        $ret = $query->fetchAll();
+        if (!$ret || count($ret)!=1) {
+            $this->errno = -1003;
+            $this->errmsg = '用户查找失败';
+            return false;
+        }
+        $userInfo = $ret[0];
+        if ($this->_password_generate($pwd) != $userInfo['pwd']){ // 校验密码
+            $this->errno = -1004;
+            $this->errmsg = '密码错误';
+            return false;
+        }
+        return intval($userInfo[1]); // 返回int类型
     }
 
     public function register($uname, $pwd) {
+        $query = $this->_db->prepare('select count(*) as c from `user` WHERE `name`= ? '); // 备要执行的SQL语句并返回一个 PDOStatement 对象
+        $query->execute(array($uname)); // 执行一条预处理语句, 把$uname 赋值到 ?
+        $count = $query->fetchAll(); // 返回一个包含结果集中所有行的数组
+        if ($count[0]['c'] > 0) {
+            $this->errno = -1005;
+            $this->errmsg = '用户名已存在';
+            return false;
+        }
+
+        if (strlen($pwd) < 8) {
+            $this->errno = -1006;
+            $this->errmsg = '密码太短，请设置至少8位的密码';
+            return false;
+        } else {
+            $password = $this->_password_generate($pwd); // 生成32位的MD5加密字符串
+        }
+        $query = $this->_db->prepare('insert into `user` (`name`, `pwd`,`reg_time`) VALUES (?, ?, ?)');
+        $ret = $query->execute(array($uname, $password, date('Y-m-d H:i:s')));
+        if (!$ret){
+            $this->errno = -1007;
+            $this->errmsg = '注册失败，写入数据失败';
+            return false;
+        }
+
         return true;
+    }
+
+    // MD5加密
+    private function _password_generate($password){
+        $pwd = md5('salt-lsh'.$password);
+        return $pwd;
     }
 }
